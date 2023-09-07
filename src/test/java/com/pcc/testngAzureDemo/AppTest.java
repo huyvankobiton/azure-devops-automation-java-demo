22cc767dfc4abe1284999477ffb729a06642eb39
huyvan <huyvan@kobiton.com>
2023-09-07 17:15:33 +0700

    change structure
    change structure

--- src/test/java/com/pcc/testngAzureDemo/AppTest.java
+++ src/test/java/com/pcc/testngAzureDemo/AppTest.java
@@ -1,37 +1,33 @@
 package com.pcc.testngAzureDemo;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.AfterSuite;
 
 import org.testng.annotations.Test;
+import org.testng.Assert;
 import io.appium.java_client.MobileElement;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class AppTest {
-  public static URL url;
+public class AppTest extends BaseTest {
   public static DesiredCapabilities capabilities;
   public static AndroidDriver<MobileElement> driver;
-  //@Parameters({ deviceName_,"platformVersion_"})
   @BeforeSuite
   public void setupAppium() throws MalformedURLException {
-    //Run test on Kobiton
-	  
-	  
-    //The generated session will be visible to you only. 
-    String kobitonServerUrl = "https://" + "kobitonadmin+businesscloud" + ":" + "884f4fc8-3fbe-42d0-90f3-3fa706d6554a" + "@api.kobiton.com/wd/hub";
+    super.Setup();
+
     DesiredCapabilities capabilities = new DesiredCapabilities();
-    // The generated session will be visible to you only. 
     capabilities.setCapability("sessionName", "PMO test executor");
     capabilities.setCapability("sessionDescription", "automation");
     capabilities.setCapability("deviceOrientation", "portrait");
@@ -41,68 +37,51 @@ public class AppTest {
     capabilities.setCapability("useConfiguration", "");
     capabilities.setCapability("autoWebview", true);
     capabilities.setCapability("browserName", "chrome");
-    // The maximum size of application is 500MB
-    // By default, HTTP requests from testing library are expired
-    // in 2 minutes while the app copying and installation may
-    // take up-to 30 minutes. Therefore, you need to extend the HTTP
-    // request timeout duration in your testing library so that
-    // it doesn't interrupt while the device is being initialized.
-    // String app = System.getenv("KOBITON_SESSION_APPLICATION") != null ? System.getenv("KOBITON_SESSION_APPLICATION") : "kobiton-store:91041";
-    // capabilities.setCapability("app", app);
-
-    capabilities.setCapability("groupId", 3159); // Group: ROYAL
+    capabilities.setCapability("groupId", 3159);
     capabilities.setCapability("deviceGroup", "ORGANIZATION");
-    // For deviceName, platformVersion Kobiton supports wildcard
-    // character *, with 3 formats: *text, text* and *text*
-    // If there is no *, Kobiton will match the exact text provided
-    
-    // String deviceName = System.getenv("KOBITON_DEVICE_NAME") != null ? System.getenv("KOBITON_DEVICE_NAME") : "Galaxy*";
-    // String platformVersion = System.getenv("KOBITON_SESSION_PLATFORM_VERSION") != null ? System.getenv("KOBITON_SESSION_PLATFORM_VERSION") : "6*";
-    // String platformName = System.getenv("KOBITON_DEVICE_PLATFORM_NAME") != null ? System.getenv("KOBITON_DEVICE_PLATFORM_NAME") : "android";
-    
-    // capabilities.setCapability("deviceName", deviceName);
-    // capabilities.setCapability("platformVersion", platformVersion);
-    // capabilities.setCapability("platformName", platformName);
     capabilities.setCapability("udid", "R3CN20NFKMM");
-    // capabilities.setCapability("deviceName", "Galaxy Note20 5G");
-    // capabilities.setCapability("tagName", "");
-    // capabilities.setCapability("platformVersion", "11");
-    // capabilities.setCapability("platformName", "Android");
-    // capabilities.setCapability("kobi:retainDurationInSeconds", 0);
 	  
     System.out.println(capabilities);
 
-    url = new URL(kobitonServerUrl);
-
-    driver = new AndroidDriver<MobileElement>(url, capabilities);
+    driver = new AndroidDriver<MobileElement>(getAutomationUrl(), capabilities);
     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
   }
 
   @AfterSuite
-  public void uninstallApp() throws InterruptedException {
+  public void uninstallApp() throws InterruptedException, IOException {
     System.out.println("After Suite");
     String kobitonSessionId = driver.getSessionDetails().get("kobitonSessionId").toString();
     String kobitonDeviceName = driver.getSessionDetails().get("deviceName").toString();
     System.out.println("getPageSource: " + driver.getPageSource());
     System.out.println("getTitle: " + driver.getTitle());
     System.out.println("getCapabilities: " + driver.getCapabilities());
-		System.out.println("getSessionDetails: " + driver.getSessionDetails());
-		System.out.println("getSessionId: " + driver.getSessionId());
-		System.out.println("getAutomationName: " + driver.getAutomationName());
-		System.out.println("getCommandExecutor: " + driver.getCommandExecutor());
-		System.out.println("getErrorHandler: " + driver.getErrorHandler());
-		System.out.println("getPlatformName: " + driver.getPlatformName());
-		System.out.println("getRemoteAddress: " + driver.getRemoteAddress());
-		System.out.println("getExecuteMethod: " + driver.getExecuteMethod());
-    // System.out.println("kobitonSessionId: " + kobitonSessionId);
-    // System.out.println("getCommandExecutor: " + driver.getCommandExecutor());
-    System.out.println("# Test Execution Log - Bitrise");
+    System.out.println("getSessionDetails: " + driver.getSessionDetails());
+    System.out.println("getSessionId: " + driver.getSessionId());
+    System.out.println("getAutomationName: " + driver.getAutomationName());
+    System.out.println("getCommandExecutor: " + driver.getCommandExecutor());
+    System.out.println("getErrorHandler: " + driver.getErrorHandler());
+    System.out.println("getPlatformName: " + driver.getPlatformName());
+    System.out.println("getRemoteAddress: " + driver.getRemoteAddress());
+    System.out.println("getExecuteMethod: " + driver.getExecuteMethod());
 
-    long yourmilliseconds = System.currentTimeMillis();
+    long timeMillis = System.currentTimeMillis();
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");    
-    Date resultdate = new Date(yourmilliseconds);
+    Date resultdate = new Date(timeMillis);
     System.out.println("**Date:** " + sdf.format(resultdate));
 
+    String url = String.format("https://%s/v1/sessions/%s", getHostName(), kobitonSessionId);
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+            .url(url)
+            .header("Authorization", getAuthorization())
+            .build();
+    Response response = client.newCall(request).execute();
+    Assert.assertNotNull(response.body(), "Response body is null");
+    String responseBodyString = response.body().string();
+    System.out.println("response body: " + responseBodyString);
+    Assert.assertNotNull(responseBodyString, "Response body string is null");
+
+    System.out.println("# Test Execution Log - Bitrise");
     System.out.println("### Test Device Used");
     System.out.println("  - " + kobitonDeviceName + " (" + driver.getPlatformName() + " " + driver.getSessionDetails().get("platformVersion").toString() + ") " + "- UIID: " + driver.getSessionDetails().get("udid").toString());
     System.out.println("  - Session URL: " + driver.getRemoteAddress() + "/" + kobitonSessionId);
---
+++ src/test/java/com/pcc/testngAzureDemo/BaseTest.java
@@ -0,0 +1,75 @@
+/*
+ * Created on Sep 28, 2017, 11:55:54 AM
+ *
+ * Copyright(c) 2017 Kobiton Inc.  All Rights Reserved.
+ * This software is the proprietary information of Kobiton Company.
+ *
+ */
+
+package com.pcc.testngAzureDemo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import org.testng.annotations.BeforeTest;
+import static org.testng.util.Strings.isNullOrEmpty;
+
+public class BaseTest extends TestListenerAdapter {
+    private String automationUrl;
+    private final String username;
+    private final String apiKey;
+    private String hostName = "api.kobiton.com";
+
+    public BaseTest() {
+        this.username = "kobitonadmin+businesscloud";
+        this.apiKey = "884f4fc8-3fbe-42d0-90f3-3fa706d6554a";
+
+        String envHostName = "@api.kobiton.com";
+        if (envHostName != null) {
+            this.hostName = envHostName;
+        }
+
+        if (this.username != null && this.apiKey != null) {
+            this.automationUrl = String.format("https://%s:%s@%s/wd/hub", username, apiKey, hostName);
+        }
+    }
+
+    @BeforeTest
+    protected void Setup() throws Exception {
+        if (isNullOrEmpty(username)) {
+            Assert.fail("Username was missing");
+        }
+
+        if (isNullOrEmpty(apiKey)) {
+            Assert.fail("APIKey was missing");
+        }
+    }
+
+    @Override
+    public void onTestFailure(ITestResult itr) {
+        super.onTestFailure(itr);
+
+        System.out.println("Test failed!");
+        if (itr.getThrowable() != null) {
+            itr.getThrowable().printStackTrace();
+        }
+    }
+
+    protected URL getAutomationUrl() throws MalformedURLException {
+        return new URL( automationUrl);
+    }
+
+    protected String getHostName() {
+        return hostName;
+    }
+    
+    protected String getAuthorization() {
+        String keySource = String.format("%s:%s", this.username, this.apiKey);
+        String base64Key = Base64.getEncoder().encodeToString(keySource.getBytes());
+        
+        return String.format("Basic %s", base64Key);
+    }
+}
